a26a7de038ff36592f4d13468f303784edb1c2e7
Kurt Zikaras <kurtzikaras@gmail.com>
2019-04-20 12:30:37 -0400

    added session and widget functionality

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
+from passlib.hash import sha256_crypt
 import os
 app = Flask(__name__)
 
@@ -21,10 +22,24 @@ class Registration(db.Model):
     password = db.Column(db.String, nullable=False)
 
 
+class Widget(db.Model):
+    __tablename__ = 'widget'
+    widget_id = db.Column(db.Integer, primary_key=True)
+    reg_id = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(120), nullable=False)
+
+
 @app.route('/')
 def index():
-    user_logged_in = False
-    return render_template('index.html', user_logged_in=user_logged_in)
+    if 'user_email' in session.keys():
+        print(session)
+        user_logged_in = True
+        widgets = Widget.query.filter_by(reg_id=session['user_id']).all()
+        print(widgets)
+    else:
+        widgets = []
+        user_logged_in = False
+    return render_template('index.html', user_logged_in=user_logged_in, widgets=widgets)
 
 
 @app.route('/login')
@@ -32,16 +47,32 @@ def login():
     print(request)
     user_email = request.args['email']
     user_password = request.args['password']
-    # user is an object returned from PG
-    user = Registration.query.filter_by(email=user_email).first()
+    try:
+        # user is an object returned from PG
+        user = Registration.query.filter_by(email=user_email).first()
+    except:
+        return render_template('index.html', user_logged_in=False, message="Error logging in")
     email = user.email
     password = user.password
     print(email)
     print(password)
-    if user_email == email and user_password == password:
-        return render_template('index.html', user_logged_in=True, message="logged in")
+    if user_email == email and sha256_crypt.verify(user_password, password):
+        session['user_email'] = user_email
+        session['user_id'] = user.id
+        session['logged_in'] = True
+        print(session)
+        widgets = Widget.query.filter_by(reg_id=session['user_id']).all()
+        return render_template('index.html', user_logged_in=session['logged_in'], message="logged in", widgets=widgets)
     else:
-        return render_template('index.html', user_logged_in=False, message="Error logging in")
+        return render_template('index.html', user_logged_in=session['logged_in'], message="Error logging in")
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    print(session)
+    user_logged_in = False
+    return render_template('index.html', user_logged_in=user_logged_in)
 
 
 @app.route('/signupform')
@@ -52,11 +83,26 @@ def signupform():
 @app.route('/signup')
 def signup():
     user_email = request.args['email']
-    user_password = request.args['password']
-    new_user = Registration(id=2, email=user_email, password=user_password)
+    user_password = sha256_crypt.encrypt(request.args['password'])
+    new_user = Registration(email=user_email, password=user_password)
     db.session.add(new_user)
     db.session.commit()
     return render_template('index.html', message="You are registered!")
+
+
+@app.route('/widget')
+def widgetform():
+    return render_template('widget_form.html')
+
+
+@app.route('/create_widget')
+def create_widget():
+    new_widget = Widget(
+        description=request.args['description'], reg_id=session['user_id'])
+    db.session.add(new_widget)
+    db.session.commit()
+    widgets = Widget.query.filter_by(reg_id=session['user_id']).all()
+    return render_template('index.html', user_logged_in=session['logged_in'], message="Widget created successfully!", widgets=widgets)
 
 
 if __name__ == '__main__':
